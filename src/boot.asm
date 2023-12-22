484408b236978bb5e617e0594e5329039328656a
spatialflunky1 <mjandrus06@gmail.com>
2023-12-22 03:48:11 -0500

    Added printing functionality to the bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -1,12 +1,17 @@
 %include "boot.inc"
 
+mov bx,message
+mov cx,message_len
+call print
+
 halt: hlt
 
 ; Functions (after hlt so they wont start executing after main instructions end)
-
+%include "func16.inc"
 
 ; Data section
-message db "Hello World",0Dh,0Ah,0
+message db "Hello World",0Dh,0Ah
+message_len EQU $ - message ; add `align 2` before this macro if more data is added
 
 ; Magic section
 times 510-($-$$) db 0
