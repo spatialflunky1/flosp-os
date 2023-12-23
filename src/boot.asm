a98b4e94da52804b08ea7f3ea86ea6f54be92b65
spatialflunky1 <mjandrus06@gmail.com>
2023-12-23 10:42:50 -0500

    Better idle loop and boot text

--- src/boot.asm
+++ src/boot.asm
@@ -1,17 +1,18 @@
 %include "boot.inc"
 
-mov bx,message
-mov cx,message_len
+mov bx,bootmsg
+mov cx,bootmsg_len
 call print
 
-halt: hlt
+halt: hlt ; better idle loop
+jmp halt
 
 ; Functions (after hlt so they wont start executing after main instructions end)
 %include "func16.inc"
 
 ; Data section
-message db "Hello World",0Dh,0Ah
-message_len EQU $ - message ; add `align 2` before this macro if more data is added
+bootmsg db "Booting Flosp...",0Dh,0Ah
+bootmsg_len EQU $ - bootmsg ; add `align 2` before this macro if more data is added
 
 ; Magic section
 times 510-($-$$) db 0
