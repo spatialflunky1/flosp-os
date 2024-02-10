69bdf3ec1d25e2184d39b03b73c0988f080cc243
spatialflunky1 <mjandrus06@gmail.com>
2024-02-09 22:09:23 -0500

    used string concatenation for simplification

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,8 +5,6 @@
 void main(void) {
     reset_output_8024();
     disable_cursor();
-    kprint_8024("Kernel flosp-");
-    kprint_8024(KVER);
-    kprint_8024(" loaded\n\0");
+    kprint_8024("Kernel flosp-"KVER" loaded\n\0");
     for(;;);
 }
