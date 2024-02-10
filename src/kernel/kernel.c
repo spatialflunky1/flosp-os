f597c5d26058e06ca8f60aadbd7e6f8943a47330
spatialflunky1 <mjandrus06@gmail.com>
2024-02-09 22:12:22 -0500

    c strings by default are null terminated

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -5,6 +5,6 @@
 void main(void) {
     reset_output_8024();
     disable_cursor();
-    kprint_8024("Kernel flosp-"KVER" loaded\n\0");
+    kprint_8024("Kernel flosp-"KVER" loaded\n");
     for(;;);
 }
